d9bfd3f5594b93b76d7ef62930fb16614b4cfa3f
LucaB98 <lucabellan98@gmail.com>
2024-02-01 20:38:18 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,51 @@
 
 <script>
-export default {
+import AppHeader from './components/AppHeader.vue';
 
-}
+export default {
+  name: 'Comics',
+  components: { AppHeader }
+};
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+</template>
 
-<style></style>
+<style>
+/* reset */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* generics */
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+body {
+  min-height: 100vh;
+  background-color: black;
+}
+
+/* utils */
+
+.container {
+  margin: 0 auto;
+  max-width: 1200px;
+
+}
+
+.flex {
+  display: flex;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,109 @@
+
+<script>
+export default {
+    name: 'AppHeader',
+    data: () => ({
+        links: [
+            {
+                text: 'Characters',
+                url: '#',
+                active: false,
+            },
+            {
+                text: 'Comics',
+                url: '#',
+                active: true,
+            },
+            {
+                text: 'Movies',
+                url: '#',
+                active: false,
+            },
+            {
+                text: 'TV',
+                url: '#',
+                active: false,
+            },
+            {
+                text: 'Games',
+                url: '#',
+                active: false,
+            },
+            {
+                text: 'Collectibles',
+                url: '#',
+                active: false,
+            },
+            {
+                text: 'Videos',
+                url: '#',
+                active: false,
+            },
+            {
+                text: 'Fans',
+                url: '#',
+                active: false,
+            },
+            {
+                text: 'News',
+                url: '#',
+                active: false,
+            },
+            {
+                text: 'Shop',
+                url: '#',
+                active: false,
+            },
+        ],
+    })
+};
+</script>
+
+<template>
+    <header id="header">
+        <div class="container">
+            <section class="nav-header">
+                <img src="../assets/img/dc-logo.png" alt="logo">
+                <nav>
+                    <ul class="flex">
+                        <li v-for="(link, i) in links" :key="i"><a :href="link.url" :class="{ 'active': link.active }">{{
+                            link.text }}</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </div>
+    </header>
+</template>
+
+<style>
+#header {
+    background-color: white;
+}
+
+.nav-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 1.5rem 0;
+}
+
+.nav-header img {
+    margin: 15px;
+    max-width: 100px;
+}
+
+ul li {
+    list-style-type: none;
+}
+
+ul li a {
+    padding: 10px;
+    margin: 5px;
+
+}
+
+ul li a .active,
+ul li a:hover {
+    color: #0C7CEC;
+}
+</style>
